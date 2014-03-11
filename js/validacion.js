9932b8bd3277bb15e46c5c5d2269ea86541313a9
rohwinu <rohwinu@live.com>
2014-03-11 09:31:22 -0500

    c2

--- js/validacion.js
+++ js/validacion.js
@@ -3,7 +3,7 @@ function prueba()
     /*var nombre = $("#nombre").val();
     alert("Su nombre es:"+nombre);
     $("#contenido").html("<button onclick='salir();return false;'>salir</button>");*/
-    $("#contenido").load("https://www.google.com.co");
+    $("#contenido").html('<iframe id="frame2" src="http://gatolocostudios.com/MobileCDC/" width="600" height="400" scrolling="auto"> Texto alternativo para navegadores que no aceptan iframes </iframe>');
 }
 
 function salir()
